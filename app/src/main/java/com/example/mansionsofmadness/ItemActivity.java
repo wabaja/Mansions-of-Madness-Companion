d1e3d8d692ed3744caf832453c9232184b2934c2
Wabaja <7401837+wabaja@users.noreply.github.com>
2021-08-08 18:48:55 -0400

    Created a second page (items). Added page navigation. Added main screen elements.

---
+++ app/src/main/java/com/example/mansionsofmadness/ItemActivity.java
@@ -0,0 +1,17 @@
+package com.example.mansionsofmadness;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//import androidx.appcompat.widget.Toolbar;
+// import android.widget.Toolbar;
+
+public class ItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item);
+    }
+}
--- app/src/main/java/com/example/mansionsofmadness/MainActivity.java
+++ app/src/main/java/com/example/mansionsofmadness/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.mansionsofmadness;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void changePage(View view) {
+        Intent intent = new Intent(this,ItemActivity.class);
+        startActivity(intent);
+
     }
 }
