6b915c369824e1fa68cd62d19fc9a04434ca2b5a
Wabaja <7401837+wabaja@users.noreply.github.com>
2021-08-08 20:14:46 -0400

    Added a pop up menu off of the character portrait.  Added a new class for the character information.  Added numeric outputs for stats.

--- app/src/main/java/com/example/mansionsofmadness/MainActivity.java
+++ app/src/main/java/com/example/mansionsofmadness/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.mansionsofmadness;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Application;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
-
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,11 +19,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton imageButton;
+        imageButton = (ImageButton) findViewById(R.id.characterImageButton);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu charPopUpMenu = new PopupMenu(MainActivity.this,imageButton);
+                charPopUpMenu.getMenuInflater().inflate(R.menu.character_menu, charPopUpMenu.getMenu());
+                charPopUpMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        return false;
+                    }
+                });
+                charPopUpMenu.show();
+            }
+        });
     }
 
     public void changePage(View view) {
         Intent intent = new Intent(this,ItemActivity.class);
         startActivity(intent);
-
     }
+
+
+
+    // Declare all character objects
+
 }
+
---
+++ app/src/main/java/com/example/mansionsofmadness/characterInfo.java
@@ -0,0 +1,19 @@
+package com.example.mansionsofmadness;
+
+import android.content.Intent;
+
+// Class to store each characters information
+public class characterInfo {
+    String name             = "";
+    String image            = "";
+    String bio              = "";
+    String ability          = "";
+
+    Integer stat_Health     = 0;
+    Integer stat_Sanity     = 0;
+    Integer stat_Strength   = 0;
+    Integer stat_Agility    = 0;
+    Integer stat_Lore       = 0;
+    Integer stat_Influence  = 0;
+    Integer stat_Will       = 0;
+}
