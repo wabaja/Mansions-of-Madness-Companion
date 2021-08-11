76f1132bc28780976a44a28e41c14bec69c512f6
Wabaja <7401837+wabaja@users.noreply.github.com>
2021-08-10 20:46:04 -0400

    Created a csv parser, loaded all character info to a class, and enabled the pop up selection to changed the output values.

--- app/src/main/java/com/example/mansionsofmadness/MainActivity.java
+++ app/src/main/java/com/example/mansionsofmadness/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.Application;
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -13,15 +14,135 @@ import android.widget.ImageButton;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public static characterInfo[] characterArray = new characterInfo[8];
+
+    public static characterInfo selectedCharacter = new characterInfo();
+
+    public void readDataFiles(){
+        //Initialize the class objects in the array
+        characterArray[0] = new characterInfo();
+        characterArray[1] = new characterInfo();
+        characterArray[2] = new characterInfo();
+        characterArray[3] = new characterInfo();
+        characterArray[4] = new characterInfo();
+        characterArray[5] = new characterInfo();
+        characterArray[6] = new characterInfo();
+        characterArray[7] = new characterInfo();
+
+
+        //This line opens the file and returns an Input Stream data type
+        InputStream charIs = getResources().openRawResource(R.raw.character_csv);
+
+        //Read the file line by line
+        BufferedReader reader = new BufferedReader(new InputStreamReader(charIs, Charset.forName("UTF-8")));
+
+        //Initialize a var to track the line of the file being read in
+        String line ="";
+
+        // error proofing if the file is weird
+        try {
+            int i = 0;
+            while ((line = reader.readLine()) != null){
+                //split data by commas into an array of strings
+                String[] token = line.split(",");
+
+                //Read the data
+
+                characterArray[i].setName(token[0]);
+                characterArray[i].setAbility(token[1]);
+                characterArray[i].setBio(token[2]);
+                characterArray[i].setStat_Health(Integer.parseInt(token[3]));
+                characterArray[i].setStat_Sanity(Integer.parseInt(token[4]));
+                characterArray[i].setStat_Strength(Integer.parseInt(token[5]));
+                characterArray[i].setStat_Agility(Integer.parseInt(token[6]));
+                characterArray[i].setStat_Observation(Integer.parseInt(token[7]));
+                characterArray[i].setStat_Lore(Integer.parseInt(token[8]));
+                characterArray[i].setStat_Influence(Integer.parseInt(token[9]));
+                characterArray[i].setStat_Will(Integer.parseInt(token[10]));
+
+                Log.d("My Activity", "Just added: "+characterArray[i]);
+            }
+
+        } catch (IOException e) {
+            Log.wtf("MyActivity","Error reading data file on line"+line,e);
+            e.printStackTrace();
+        }
+    }
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ////////////////////////////////////////////////
+        //Initialize the class objects in the array
+        characterArray[0] = new characterInfo();
+        characterArray[1] = new characterInfo();
+        characterArray[2] = new characterInfo();
+        characterArray[3] = new characterInfo();
+        characterArray[4] = new characterInfo();
+        characterArray[5] = new characterInfo();
+        characterArray[6] = new characterInfo();
+        characterArray[7] = new characterInfo();
+
+
+        //This line opens the file and returns an Input Stream data type
+        InputStream charIs = getResources().openRawResource(R.raw.character_csv);
+
+        //Read the file line by line
+        BufferedReader reader = new BufferedReader(new InputStreamReader(charIs, Charset.forName("UTF-8")));
+
+        //Initialize a var to track the line of the file being read in
+        String line ="";
+
+        // error proofing if the file is weird
+        try {
+            int i = 0;
+            while ((line = reader.readLine()) != null){
+                //split data by commas into an array of strings
+                String[] token = line.split(",");
+
+                //Read the data
+
+                characterArray[i].setName(token[0]);
+                characterArray[i].setAbility(token[1]);
+                characterArray[i].setBio(token[2]);
+                characterArray[i].setStat_Health(Integer.parseInt(token[3]));
+                characterArray[i].setStat_Sanity(Integer.parseInt(token[4]));
+                characterArray[i].setStat_Strength(Integer.parseInt(token[5]));
+                characterArray[i].setStat_Agility(Integer.parseInt(token[6]));
+                characterArray[i].setStat_Observation(Integer.parseInt(token[7]));
+                characterArray[i].setStat_Lore(Integer.parseInt(token[8]));
+                characterArray[i].setStat_Influence(Integer.parseInt(token[9]));
+                characterArray[i].setStat_Will(Integer.parseInt(token[10]));
+
+                Log.d("My Activity", "Just added: "+characterArray[i]);
+
+                i = i + 1;
+            }
+
+        } catch (IOException e) {
+            Log.wtf("MyActivity","Error reading data file on line"+line,e);
+            e.printStackTrace();
+        }
+
+
+        /////////////////////////////////////////////
         setContentView(R.layout.activity_main);
 
+
+        //Initialize the character selector
+        //selectedCharacter = characterArray[0];
         // Enables a drop down list from the character portrait
         ImageButton imageButton;
         imageButton = (ImageButton) findViewById(R.id.characterImageButton);
@@ -34,6 +155,63 @@ public class MainActivity extends AppCompatActivity {
                 charPopUpMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
                     @Override
                     public boolean onMenuItemClick(MenuItem menuItem) {
+                        switch (menuItem.getTitle().toString()){
+                            case "Agatha Crane":
+                                selectedCharacter = characterArray[0];
+                                break;
+
+                            case "Carson Sinclair":
+                                selectedCharacter = characterArray[1];
+                                break;
+
+                            case "Father Matteo":
+                                selectedCharacter = characterArray[2];
+                                break;
+
+                            case "Minh Thi Phan":
+                                selectedCharacter = characterArray[3];
+                                break;
+
+                            case "Preston Fairmont":
+                                selectedCharacter = characterArray[4];
+                                break;
+
+                            case "Rita Young":
+                                selectedCharacter = characterArray[5];
+                                break;
+
+                            case "Wendy Adams":
+                                selectedCharacter = characterArray[6];
+                                break;
+
+                            case "William Yorick":
+                                selectedCharacter = characterArray[7];
+                                break;
+                        }
+
+                        // Create links to the output boxes
+                        TextView text_name = (TextView) findViewById(R.id.heading_Name);
+                        TextView text_bio = (TextView) findViewById(R.id.heading_Bio);
+
+
+                        TextView text_strength = (TextView) findViewById(R.id.output_strength);
+                        TextView text_agility = (TextView) findViewById(R.id.output_agility);
+                        TextView text_will = (TextView) findViewById(R.id.output_will);
+                        TextView text_lore = (TextView) findViewById(R.id.output_lore);
+                        TextView text_observation = (TextView) findViewById(R.id.output_observation);
+                        TextView text_influence = (TextView) findViewById(R.id.output_influence);
+
+                        //Write the output values to the boxes
+                        text_name.setText(selectedCharacter.name);
+                        text_bio.setText(selectedCharacter.bio);
+
+                        text_strength.setText(""+selectedCharacter.stat_Strength);
+                        text_agility.setText(""+selectedCharacter.stat_Agility);
+                        text_will.setText(""+selectedCharacter.stat_Will);
+                        text_lore.setText(""+selectedCharacter.stat_Lore);
+                        text_observation.setText(""+selectedCharacter.stat_Observation);
+                        text_influence.setText(""+selectedCharacter.stat_Influence);
+
                         return false;
                     }
                 });
@@ -41,73 +219,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // Create the array of character info
-        characterInfo[] characterArray = new characterInfo[8];
-        //Initialize the class objects in the array
-        characterArray[0] = new characterInfo();
-        characterArray[1] = new characterInfo();
-        characterArray[2] = new characterInfo();
-        characterArray[3] = new characterInfo();
-        characterArray[4] = new characterInfo();
-        characterArray[5] = new characterInfo();
-        characterArray[6] = new characterInfo();
-        characterArray[7] = new characterInfo();
-
-        //Map the selected array element to the transit variable
-        characterInfo selectedCharacter = new characterInfo();
-        selectedCharacter = characterArray[0];
-
-        // Populate character info
-        // Agatha Crane
-        characterArray[0].name = "Agatha Crane";
-        characterArray[0].bio = "The Parapsychologist";
-        characterArray[0].ability = "After you resolve a horror check, gain 1 Clue";
-        characterArray[0].image = "";
-        characterArray[0].stat_Health = 5;
-        characterArray[0].stat_Sanity = 9;
-        characterArray[0].stat_Strength = 2;
-        characterArray[0].stat_Agility = 3;
-        characterArray[0].stat_Lore = 5;
-        characterArray[0].stat_Will = 4;
-        characterArray[0].stat_Influence = 3;
-        characterArray[0].stat_Observation = 4;
-
-        //AWilliam Yorick
-        characterArray[1].name = "William Yorick";
-        characterArray[1].bio = "The Parapsychologist";
-        characterArray[1].ability = "Whenever a monster is defeated, gain one clue.";
-        characterArray[1].image = "";
-        characterArray[1].stat_Health = 7;
-        characterArray[1].stat_Sanity = 7;
-        characterArray[1].stat_Strength = 4;
-        characterArray[1].stat_Agility = 3;
-        characterArray[1].stat_Lore = 3;
-        characterArray[1].stat_Will = 4;
-        characterArray[1].stat_Influence = 4;
-        characterArray[1].stat_Observation = 3;
-
-        // Create links to the output boxes
-        TextView text_name = (TextView) findViewById(R.id.heading_Name);
-        TextView text_bio = (TextView) findViewById(R.id.heading_Bio);
-
-
-        TextView text_strength = (TextView) findViewById(R.id.output_strength);
-        TextView text_agility = (TextView) findViewById(R.id.output_agility);
-        TextView text_will = (TextView) findViewById(R.id.output_will);
-        TextView text_lore = (TextView) findViewById(R.id.output_lore);
-        TextView text_observation = (TextView) findViewById(R.id.output_observation);
-        TextView text_influence = (TextView) findViewById(R.id.output_influence);
-
-        //Write the output values to the boxes
-        text_name.setText(selectedCharacter.name);
-        text_bio.setText(selectedCharacter.bio);
-
-        text_strength.setText(""+selectedCharacter.stat_Strength);
-        text_agility.setText(""+selectedCharacter.stat_Agility);
-        text_will.setText(""+selectedCharacter.stat_Will);
-        text_lore.setText(""+selectedCharacter.stat_Lore);
-        text_observation.setText(""+selectedCharacter.stat_Observation);
-        text_influence.setText(""+selectedCharacter.stat_Influence);
     }
 
     public void changePage(View view) {
--- app/src/main/java/com/example/mansionsofmadness/characterInfo.java
+++ app/src/main/java/com/example/mansionsofmadness/characterInfo.java
@@ -18,6 +18,122 @@ public class characterInfo {
     int stat_Observation;
     int stat_Influence;
     int stat_Will;
+
+
+    // Use Alt+insert to automatically generate this stuff
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getAbility() {
+        return ability;
+    }
+
+    public void setAbility(String ability) {
+        this.ability = ability;
+    }
+
+    public int getStat_Health() {
+        return stat_Health;
+    }
+
+    public void setStat_Health(int stat_Health) {
+        this.stat_Health = stat_Health;
+    }
+
+    public int getStat_Sanity() {
+        return stat_Sanity;
+    }
+
+    public void setStat_Sanity(int stat_Sanity) {
+        this.stat_Sanity = stat_Sanity;
+    }
+
+    public int getStat_Strength() {
+        return stat_Strength;
+    }
+
+    public void setStat_Strength(int stat_Strength) {
+        this.stat_Strength = stat_Strength;
+    }
+
+    public int getStat_Agility() {
+        return stat_Agility;
+    }
+
+    public void setStat_Agility(int stat_Agility) {
+        this.stat_Agility = stat_Agility;
+    }
+
+    public int getStat_Lore() {
+        return stat_Lore;
+    }
+
+    public void setStat_Lore(int stat_Lore) {
+        this.stat_Lore = stat_Lore;
+    }
+
+    public int getStat_Observation() {
+        return stat_Observation;
+    }
+
+    public void setStat_Observation(int stat_Observation) {
+        this.stat_Observation = stat_Observation;
+    }
+
+    public int getStat_Influence() {
+        return stat_Influence;
+    }
+
+    public void setStat_Influence(int stat_Influence) {
+        this.stat_Influence = stat_Influence;
+    }
+
+    public int getStat_Will() {
+        return stat_Will;
+    }
+
+    public void setStat_Will(int stat_Will) {
+        this.stat_Will = stat_Will;
+    }
+
+    @Override
+    public String toString() {
+        return "characterInfo{" +
+                "name='" + name + '\'' +
+                ", image='" + image + '\'' +
+                ", bio='" + bio + '\'' +
+                ", ability='" + ability + '\'' +
+                ", stat_Health=" + stat_Health +
+                ", stat_Sanity=" + stat_Sanity +
+                ", stat_Strength=" + stat_Strength +
+                ", stat_Agility=" + stat_Agility +
+                ", stat_Lore=" + stat_Lore +
+                ", stat_Observation=" + stat_Observation +
+                ", stat_Influence=" + stat_Influence +
+                ", stat_Will=" + stat_Will +
+                '}';
+    }
 }
 
 
